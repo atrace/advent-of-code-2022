fe6348191c6d5dac7a716d1f3daf13915756fd7a
anjali <anjali.trace13@gmail.com>
2022-12-03 12:35:10 +0000

    Solved day 1

--- day1/day1.ts
+++ day1/day1.ts
@@ -2,7 +2,20 @@ import * as events from "events";
 import * as fs from "fs";
 import * as readline from "readline";
 
-const processLineByLine = async ({ fileName }: { fileName: string }) => {
+const state: { currentCount: number; previousCounts: number[] } = {
+  currentCount: 0,
+  previousCounts: [],
+};
+
+interface ProcessLineByLineProps {
+  fileName: string;
+  processLine: (line: string) => void;
+}
+
+const processLineByLine = async ({
+  fileName,
+  processLine,
+}: ProcessLineByLineProps) => {
   console.log("foo2");
   try {
     const rl = readline.createInterface({
@@ -10,9 +23,7 @@ const processLineByLine = async ({ fileName }: { fileName: string }) => {
       crlfDelay: Infinity,
     });
 
-    rl.on("line", (line) => {
-      console.log(`Line from file: ${line}`);
-    });
+    rl.on("line", processLine);
 
     await events.once(rl, "close");
 
@@ -24,7 +35,35 @@ const processLineByLine = async ({ fileName }: { fileName: string }) => {
   } catch (err) {
     console.error(err);
   }
+  console.log("foo3");
+  state.previousCounts.sort((a, b) => {
+    return b - a;
+  });
+  console.log("Top calorie elf:", state.previousCounts[0]);
+
+  console.log(
+    "Sum of the top 3 caloried elves:",
+    state.previousCounts[0] + state.previousCounts[1] + state.previousCounts[2]
+  );
 };
 
 console.log("foo");
-processLineByLine({ fileName: "./day1/input.txt" });
+
+const thisDoesAThing = (line: string) => {
+  console.log(`Line from file: ${line}`);
+
+  if (line.length === 0) {
+    state.previousCounts.push(state.currentCount);
+    state.currentCount = 0;
+    return;
+  }
+
+  state.currentCount += parseInt(line);
+  console.log(state);
+};
+
+processLineByLine({
+  fileName: "./day1/input.txt",
+  processLine: thisDoesAThing,
+});
+console.log("helloooo?");
