14f6778b61fea03274f806fb7c149df07d6aea90
anjali <anjali.trace13@gmail.com>
2022-12-03 12:48:55 +0000

    Refactor to pull out file reader for reuse

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,55 +1,13 @@
-import * as events from "events";
-import * as fs from "fs";
-import * as readline from "readline";
+import { processLineByLine } from "../fileReader";
 
 const state: { currentCount: number; previousCounts: number[] } = {
   currentCount: 0,
   previousCounts: [],
 };
 
-interface ProcessLineByLineProps {
-  fileName: string;
-  processLine: (line: string) => void;
-}
-
-const processLineByLine = async ({
-  fileName,
-  processLine,
-}: ProcessLineByLineProps) => {
-  console.log("foo2");
-  try {
-    const rl = readline.createInterface({
-      input: fs.createReadStream(fileName),
-      crlfDelay: Infinity,
-    });
-
-    rl.on("line", processLine);
-
-    await events.once(rl, "close");
-
-    console.log("Reading file line by line with readline done.");
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(
-      `The script uses approximately ${Math.round(used * 100) / 100} MB`
-    );
-  } catch (err) {
-    console.error(err);
-  }
-  console.log("foo3");
-  state.previousCounts.sort((a, b) => {
-    return b - a;
-  });
-  console.log("Top calorie elf:", state.previousCounts[0]);
-
-  console.log(
-    "Sum of the top 3 caloried elves:",
-    state.previousCounts[0] + state.previousCounts[1] + state.previousCounts[2]
-  );
-};
-
 console.log("foo");
 
-const thisDoesAThing = (line: string) => {
+const processLine = (line: string) => {
   console.log(`Line from file: ${line}`);
 
   if (line.length === 0) {
@@ -62,8 +20,21 @@ const thisDoesAThing = (line: string) => {
   console.log(state);
 };
 
+const doSomeStuffWithStateAtTheEnd = () => {
+  state.previousCounts.sort((a, b) => {
+    return b - a;
+  });
+  console.log("Top calorie elf:", state.previousCounts[0]);
+
+  console.log(
+    "Sum of the top 3 caloried elves:",
+    state.previousCounts[0] + state.previousCounts[1] + state.previousCounts[2]
+  );
+};
+
 processLineByLine({
   fileName: "./day1/input.txt",
-  processLine: thisDoesAThing,
+  processLine,
+  doSomeStuffWithStateAtTheEnd,
 });
 console.log("helloooo?");
---
+++ fileReader.ts
@@ -0,0 +1,36 @@
+import * as events from "events";
+import * as fs from "fs";
+import * as readline from "readline";
+
+interface ProcessLineByLineProps {
+  fileName: string;
+  processLine: (line: string) => void;
+  doSomeStuffWithStateAtTheEnd: () => void;
+}
+
+export const processLineByLine = async ({
+  fileName,
+  processLine,
+  doSomeStuffWithStateAtTheEnd,
+}: ProcessLineByLineProps) => {
+  try {
+    const rl = readline.createInterface({
+      input: fs.createReadStream(fileName),
+      crlfDelay: Infinity,
+    });
+
+    rl.on("line", processLine);
+
+    await events.once(rl, "close");
+
+    console.log("Reading file line by line with readline done.");
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(
+      `The script uses approximately ${Math.round(used * 100) / 100} MB`
+    );
+  } catch (err) {
+    console.error(err);
+  }
+
+  doSomeStuffWithStateAtTheEnd();
+};
